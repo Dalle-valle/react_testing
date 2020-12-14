363029521b149d757a53fe43d38bde70b7c867cb
Daniel <Danieldalby83@gmail.com>
2020-12-14 10:21:11 +0100

    yeah

--- src/components/Checkamazing.js
+++ src/components/Checkamazing.js
@@ -1,20 +1,29 @@
 import React from "react";
 
 export default function Checkamazing() {
-  const spaceIt = " ";
   return (
     <form className="checkout-1">
-      <label htmlFor="smartname">Smart Name</label>
-      <input type="text" name="smartname" />
-      <label htmlFor="smartnumber">Smart Number</label>
-      <input type="number" name="smartnumber" placeholder></input>
-      <div className="datewrapper">
-        <label htmlFor="expiry">expiry</label>
-        <input type="number" name="expiry" />
-        <label htmlFor="security">Cvv</label>
-        <input type="number" name="security" />
-      </div>
-      <button type="button">Place Order</button>
+      <h2 className="information">Credit Card Information</h2>
+
+      <label htmlFor="smartname" className="label1">
+        Smart Name
+      </label>
+      <input className="cardname" type="text" name="smartname" placeholder="&nbsp;" />
+      <label htmlFor="smartnumber" className="label2">
+        Smart Number
+      </label>
+      <input className="cardnumber" type="number" name="smartnumber" placeholder="&nbsp;"></input>
+      <label htmlFor="expiry" className="label3">
+        Expiry
+      </label>
+      <input className="expiry" type="number" name="expiry" placeholder="DD/MM/YY;" />
+      <label htmlFor="security" className="label4">
+        Cvv
+      </label>
+      <input className="cvv" type="number" name="security" placeholder="&nbsp;" />
+      <button className="submit-button" type="button">
+        Place Order
+      </button>
     </form>
   );
 }
